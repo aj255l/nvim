ed6edd5d65a01e49e740284260de8d57b27bd429
Anna Lee <annalee@andrew.cmu.edu>
2026-02-18 17:22:28 -0500

    Add stylua

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,11 +1,11 @@
 return {
-  'stevearc/conform.nvim',
+  "stevearc/conform.nvim",
   opts = {
     format_on_save = {
       -- These options will be passed to conform.format()
       timeout_ms = 500,
       lsp_format = "fallback",
     },
-    { formatters_by_ft = { lua = { "stylua" }, } }
+    { formatters_by_ft = { lua = { "stylua" } } },
   },
 }
